83574da6db95e7f268052a84826c296c44c6757f
JamesRobinson160552 <95374382+JamesRobinson160552@users.noreply.github.com>
2023-01-25 13:10:22 -0400

    Added enemy attack

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    float moveSpeed; 
-    public Rigidbody2D rb;
+    private float moveSpeed; 
+    public float attackRange;
+    public float attackSpeed = 0.75f;
+    public int damage = 1;
+    public float bulletForce = 10.0f;
+    public GameObject attackPrefab;
+    Rigidbody2D rb;
     Transform target_;
     Vector2 moveDirection_;
+    Vector3 direction_; //Towards player
     Unit unit_;
 
     private void Awake()
@@ -17,15 +23,17 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
         target_ = GameObject.Find("Player").transform;
         moveSpeed = unit_.GetUnitBase.MoveSpeed;
+        InvokeRepeating("Shoot", 0.0f, attackSpeed);
     }
 
     private void Update()
     {
         if(target_)
         {
-            Vector3 direction_ = (target_.position - transform.position).normalized;
+            direction_ = (target_.position - transform.position).normalized;
             moveDirection_ = direction_;
         }
     }
@@ -35,6 +43,23 @@ public class Enemy : MonoBehaviour
         if(target_)
         {
             rb.velocity = new Vector2(moveDirection_.x, moveDirection_.y) * moveSpeed;
+        }
+    }
+
+    private void Shoot() {
+        if ((direction_).magnitude <= attackRange) //Player is in range
+        {
+            // Instantiates bullet at location of aimer
+            GameObject bullet = Instantiate(attackPrefab, transform.position, attackPrefab.transform.rotation);
+
+            // Access the bullet's rigidbody and store it as rb
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+            //give the projectile the stats from the sepll
+            bullet.GetComponent<ProjectileStats>().SetDamage(damage);
+
+            // Add force to the newly instantiated rb
+            rb.AddForce(direction_ * bulletForce, ForceMode2D.Impulse);
         }
     }
 
