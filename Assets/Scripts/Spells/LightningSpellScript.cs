22f144f5aac6ed00bf784e5fe3e516d6722d707a
pjadczyk <69324953+pjadczyk@users.noreply.github.com>
2023-02-02 23:43:16 -0400

    Lightning spell
    - Lightning spell (Incomplete)
    - Still trying to have it grow over 3 seconds to full size. Currently freezing entire game (inc enemies) during cast.

---
+++ Assets/Scripts/Spells/LightningSpellScript.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class LightningSpellScript : MonoBehaviour, SpellBase
+{
+    public List<char> spellActivate = new List<char> { 'D', 'A', 'D', 'W' };
+    public string spellName = "Lightning";
+    public GameObject[] spellPrefabs;
+    public GameObject plr;
+    private float castLoop = 0.0f;
+    private float castTime = 3.0f;
+    private float BaseTime;
+    private bool lightningCast = false;
+    private bool BaseTimeSet = false;
+    private GameObject lightning;
+
+    public string getName()
+    { return spellName; }
+
+    public List<char> getSpellActivate()
+    { return spellActivate; }
+
+    public int getDamage()
+    { return 0; }
+
+    public void castSpell()
+    {
+        lightningCast = true;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Input.mousePosition;
+        mousePos = new Vector3(mousePos.x, mousePos.y, 0);
+        Debug.Log("Spell cast");
+        lightning = Instantiate(spellPrefabs[0], mousePos, spellPrefabs[0].transform.rotation);
+        Debug.Log("Spell Instantiated");
+    }
+
+    public void LateUpdate()
+    {
+        while (lightningCast == true)
+        {
+            if (BaseTimeSet == false)
+            {
+                BaseTime = Time.realtimeSinceStartup; // Create spell cast start time
+                BaseTimeSet = true;
+                Debug.Log("BaseTimerSet");
+            }
+
+            Debug.Log("BaseTimerSet");
+            if (Time.realtimeSinceStartup <= (BaseTime + castTime)) // if actual time is greater Base + castTime
+            {
+                if (Time.realtimeSinceStartup > (BaseTime + castLoop))
+                {
+                    Debug.Log(Time.realtimeSinceStartup);
+                    Debug.Log(castLoop);
+                    castLoop += 1.0f;
+                    lightning.transform.localScale = new Vector3(castLoop, castLoop, 1);
+                }
+
+                if (castLoop == castTime) // End of Loop
+                {
+                    lightningCast = false;
+                    BaseTimeSet = false;
+                    castLoop = 0.0f;
+                }
+            }
+        }
+    }
+}
