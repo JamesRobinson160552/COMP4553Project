f3cc9d4ebabb60571c9086d13406597d9281e912
pjadczyk <69324953+pjadczyk@users.noreply.github.com>
2023-02-05 18:19:57 -0400

    Lightning script updated
    - Now expands smoothly
    - Turns red for .5s to show dmg
    - Destroyed after expiring

--- Assets/Scripts/Spells/LightningSpellScript.cs
+++ Assets/Scripts/Spells/LightningSpellScript.cs
@@ -8,10 +8,11 @@ public class LightningSpellScript : MonoBehaviour, SpellBase
 {
     public List<char> spellActivate = new List<char> { 'D', 'A', 'D', 'W' };
     public string spellName = "Lightning";
+    public int damage = 0;
     public GameObject[] spellPrefabs;
     public GameObject plr;
     private float castLoop = 0.0f;
-    private float castTime = 3.0f;
+    private float castTime = 1.25f;
     private float BaseTime;
     private bool lightningCast = false;
     private bool BaseTimeSet = false;
@@ -31,9 +32,7 @@ public class LightningSpellScript : MonoBehaviour, SpellBase
         lightningCast = true;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Input.mousePosition;
         mousePos = new Vector3(mousePos.x, mousePos.y, 0);
-        Debug.Log("Spell cast");
         lightning = Instantiate(spellPrefabs[0], mousePos, spellPrefabs[0].transform.rotation);
-        Debug.Log("Spell Instantiated");
     }
 
     public void LateUpdate()
@@ -44,27 +43,31 @@ public class LightningSpellScript : MonoBehaviour, SpellBase
             {
                 BaseTime = Time.realtimeSinceStartup; // Create spell cast start time
                 BaseTimeSet = true;
-                Debug.Log("BaseTimerSet");
             }
 
-            Debug.Log("BaseTimerSet");
-            if (Time.realtimeSinceStartup <= (BaseTime + castTime)) // if actual time is greater Base + castTime
+            if (Time.realtimeSinceStartup <= (BaseTime + castTime))
+            // if actual time is <= Base + castTime (aka keep going through spell)
             {
+                // If actual time > (base + loop)
                 if (Time.realtimeSinceStartup > (BaseTime + castLoop))
                 {
-                    Debug.Log(Time.realtimeSinceStartup);
-                    Debug.Log(castLoop);
-                    castLoop += 1.0f;
-                    lightning.transform.localScale = new Vector3(castLoop, castLoop, 1);
+                    // Make loop bigger and increase size of spell
+                    castLoop += .1f;
+                    lightning.transform.localScale = new Vector3(castLoop*3, castLoop*3, 1);
                 }
-
-                if (castLoop == castTime) // End of Loop
-                {
+            }
+            if (Time.realtimeSinceStartup > (BaseTime + castTime))
+            {
+                if (Time.realtimeSinceStartup > (BaseTime + castTime + .5f)) {
+                    damage = 10;
+                    Destroy(lightning);
                     lightningCast = false;
                     BaseTimeSet = false;
                     castLoop = 0.0f;
+                } else {
+                    lightning.GetComponent<Renderer>().material.color = Color.red;
                 }
-            }
+            }            
         }
     }
 }
