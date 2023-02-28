75b8eed94979c55c97732f93765fc245411b933e
159910g <122617604+159910g@users.noreply.github.com>
2023-02-28 15:27:17 -0400

    destructible rocks

---
+++ Assets/Scripts/Utils/DestructibleObjects.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObjects : MonoBehaviour
+{
+    [SerializeField] int MaxHP;
+    [SerializeField] float radius;
+    [SerializeField]
+    
+    int damage_;
+    int currentHP_;
+
+    void Start()
+    {
+        currentHP_ = MaxHP;
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    void Update()
+    {
+        var collider = (Physics2D.OverlapCircle(transform.position, radius, GameLayers.i.PlayerSpellsLayer));
+        if(collider != null)
+        {
+            damage_ = collider.GetComponent<ProjectileStats>().getDamage();
+            currentHP_ -= damage_;
+            CameraShake.i.StopShake();
+            Destroy(collider.gameObject); //destroy projectile  
+        }
+
+        collider = (Physics2D.OverlapCircle(transform.position, radius, GameLayers.i.EnemySpellsLayer));
+        if(collider != null)
+        {
+            damage_ = collider.GetComponent<ProjectileStats>().getDamage();
+            currentHP_ -= damage_;
+            CameraShake.i.StopShake();
+            Destroy(collider.gameObject); //destroy projectile  
+        }
+
+        collider = (Physics2D.OverlapCircle(transform.position, radius, GameLayers.i.LightningLayer));
+        if(collider != null)
+        {
+            damage_ = collider.GetComponent<ProjectileStats>().getDamage();
+            currentHP_ -= damage_;
+        }
+
+        if(currentHP_ <= MaxHP/2)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if(currentHP_ <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
